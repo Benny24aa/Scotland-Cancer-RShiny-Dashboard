220d722138f85796fa02a92f1be4e75cf387c9f9
Benny24aa <harleyb101010@gmail.com>
2025-03-26 21:47:57 +0000

    Cancer Datasets for Incidence and Mortality merged to make it easier to call on for both

--- Cancer-Dashboard/Global.R
+++ Cancer-Dashboard/Global.R
@@ -1,7 +1,8 @@
 ##### Sourcing Reference Files
 source("Global/Geo File.R")
 
-
-
 #### Sourcing Download Table Lists
 source("Global/Data Download Lists.R")
+
+##### Sourcing in Cancer Data
+source("C:/Users/benny/Documents/My Resps/Scotland-Cancer-RShiny-Dashboard/Cancer-Dashboard/Global/Incidence and Mortality.R")
