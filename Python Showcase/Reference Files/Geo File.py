353eeba213a90ed0897655ece203e476ab57946f
Benny24aa <harleyb101010@gmail.com>
2025-03-26 19:01:24 +0000

    Python code added for showcasing

---
+++ Python Showcase/Reference Files/Geo File.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+HB_Lookup = HB_Lookup.drop(columns=['Country', 'HBDateEnacted']).loc[HB_Lookup['HBDateArchived'].isna()]
+HB_Lookup['GeoType'] = "Health Board"
+
+Council_Lookup = Council_Lookup[['CA', 'CAName', 'CADateArchived']].loc[Council_Lookup['CADateArchived'].isna()]
+Council_Lookup = Council_Lookup.drop(columns=['CADateArchived'])
+Council_Lookup['GeoType'] = "Council Area"
+Council_Lookup = Council_Lookup.rename(columns={'CA': 'GeoCode', 'CAName': 'GeoName'}).drop_duplicates()
+
+Interminate_Zone_Lookup = Interminate_Zone_Lookup[['IntZone', 'IntZoneName', 'HB']]
+Interminate_Zone_Lookup['GeoType'] = "Interminate Zone"
+Interminate_Zone_Lookup = Interminate_Zone_Lookup.rename(columns={'IntZone': 'GeoCode', 'IntZoneName': 'GeoName'})
+
+Hospital_Lookup = Hospital_Lookup[['HospitalCode', 'HospitalName', 'HealthBoard']]
+Hospital_Lookup['GeoType'] = "Hospital"
+Hospital_Lookup = Hospital_Lookup.rename(columns={'HospitalCode': 'GeoCode', 'HospitalName': 'GeoName', 'HealthBoard': 'HB'})
+
+Data_Zone_Lookup = Data_Zone_Lookup[['DataZone', 'DataZoneName', 'HB']]
+Data_Zone_Lookup['GeoType'] = "Data Zone"
+Data_Zone_Lookup = Data_Zone_Lookup.rename(columns={'DataZone': 'GeoCode', 'DataZoneName': 'GeoName'})
+
+Lookup_List = pd.concat([Council_Lookup, Interminate_Zone_Lookup, Hospital_Lookup, Data_Zone_Lookup], ignore_index=True)
