74f2d05fc237cc7569a1aa09f71bf7c8f9d20b1b
Benny24aa <harleyb101010@gmail.com>
2025-03-26 19:03:54 +0000

    download list added for reference file data table

---
+++ Cancer-Dashboard/Global/Data Download Lists.R
@@ -0,0 +1,5 @@
+reference_file_download_list <- c("Health Board Lookup" = "HB_Lookup",
+                                  "Council Lookup" = "Council_Lookup",
+                                  "Hospital Lookup" = "Hospital_Lookup",
+                                  "Interminate Zone Lookup" = "Interminate_Zone_Lookup",
+                                  "Data Zone Lookup" = "Data_Zone_Lookup")
