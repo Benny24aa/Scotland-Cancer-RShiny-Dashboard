8152585fabbf0bf366a432c6a2efb30286b74f86
Benny24aa <harleyb101010@gmail.com>
2025-03-26 22:57:40 +0000

    Cancer download list added to lists for filters for data table

--- Cancer-Dashboard/Global/Data Download Lists.R
+++ Cancer-Dashboard/Global/Data Download Lists.R
@@ -3,3 +3,6 @@ reference_file_download_list <- c("Health Board Lookup" = "HB_Lookup",
                                   "Hospital Lookup" = "Hospital_Lookup",
                                   "Interminate Zone Lookup" = "Interminate_Zone_Lookup",
                                   "Data Zone Lookup" = "Data_Zone_Lookup")
+
+Cancer_Download_List <- c("Cancer Incidence Data" = Cancer_Data_Incidence_Cleaned,
+                          "Cancer Data Mortality" = Cancer_Mortality_Cleaned)
