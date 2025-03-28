4efdb228f2385cff10cd28fb439fbe6abf4da46b
Benny24aa <harleyb101010@gmail.com>
2025-03-28 11:02:56 +0000

    download list updated for table

--- Cancer-Dashboard/Global/Data Download Lists.R
+++ Cancer-Dashboard/Global/Data Download Lists.R
@@ -4,5 +4,6 @@ reference_file_download_list <- c("Health Board Lookup" = "HB_Lookup",
                                   "Interminate Zone Lookup" = "Interminate_Zone_Lookup",
                                   "Data Zone Lookup" = "Data_Zone_Lookup")
 
-Cancer_Download_List <- c("Cancer Incidence Data" = Cancer_Data_Incidence_Cleaned,
-                          "Cancer Data Mortality" = Cancer_Mortality_Cleaned)
+Cancer_Download_List <- c("Cancer Incidence Data" = "Cancer_Data_Incidence_HB",
+                          "Cancer Data Mortality" = "Cancer_Data_Mortality_HB",
+                          "All Cancer and Incidence Data" = "Cancer_Full_Data")
