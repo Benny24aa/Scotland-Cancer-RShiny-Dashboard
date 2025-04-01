a9384225b2284e25e1d6f29fc5e42287b43ba4a3
Benny24aa <harleyb101010@gmail.com>
2025-04-01 19:04:38 +0100

    list changed to adjust for new dashboard structure with filters

--- Cancer-Dashboard/Global/Data Download Lists.R
+++ Cancer-Dashboard/Global/Data Download Lists.R
@@ -4,6 +4,4 @@ reference_file_download_list <- c("Health Board Lookup" = "HB_Lookup",
                                   "Interminate Zone Lookup" = "Interminate_Zone_Lookup",
                                   "Data Zone Lookup" = "Data_Zone_Lookup")
 
-Cancer_Download_List <- c("Cancer Incidence Data" = "Cancer_Data_Incidence_HB",
-                          "Cancer Data Mortality" = "Cancer_Data_Mortality_HB",
-                          "All Cancer and Incidence Data" = "Cancer_Full_Data")
+Cancer_Download_List <- c("All Cancer and Incidence Data" = "Cancer_Full_Data")
