963f1ec0170544b877336e5eec2d96fa7f77e292
Benny24aa <harleyb101010@gmail.com>
2025-04-13 11:33:28 +0100

    Changes to information UI, removing of select data type which has now been moved

--- Cancer-Dashboard/UI/Information UI.R
+++ Cancer-Dashboard/UI/Information UI.R
@@ -4,16 +4,14 @@ information <- tabPanel(title = "Home",
                         fluidRow(
                           column(6, selectInput("hb_name", label = "Select Healthboard",
                                                 choices = unique(HB_List$HBName,
-                                                                 multiple = FALSE))),
-                          
-                          column(6, selectInput("datatype_input", label = "Select data you wish to view",
-                                                choices = unique(Cancer_Data_Type$DataType,
                                                                  multiple = FALSE)))
+                          
+                      
                         ),
                         
                         fluidRow(
                           column(6,
-                                 h2("Welcome", style = "color:  #336699 ; font-weight: 600"))),
+                                 h2("Welcome to the Scottish Open Data Cancer Dashboard", style = "color:  #336699 ; font-weight: 600"))),
                         
                         fluidRow(
                           column(6, actionButton("new_next", tags$b("New content and future updates"),
@@ -26,8 +24,6 @@ information <- tabPanel(title = "Home",
                                                                                                                                                                                                                   "", target="_blank"), ), 
                                  h4(tags$b("?", style = "color:  #336699 ; font-weight: 600" )),
                                  p(""),
-                                 fluidRow(
-                                   column(3, plotlyOutput("scotland_info_graph_server", width = "400%", height = "600px"))),
                                  h4(tags$b(" Disclosure and Data Security Statement", style = "color:  #336699 ; font-weight: 600")),
                                  p("All content is available under the Open Government License V3.0, and is available on NHS Scotland Open Data except where otherwise stated. If you need any assistance with this, please visit the UK Government Website for more information regarding the Open Government License. This dashboard is not a representive of the NHS and therefore is not an official source of information.")),
                         )#End of Fluid Row
