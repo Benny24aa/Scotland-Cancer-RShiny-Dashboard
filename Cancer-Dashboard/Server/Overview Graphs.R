e11e86d78bfc1688ee196aca0a00c4a4ab2a6bc2
Benny24aa <harleyb101010@gmail.com>
2025-04-16 21:56:43 +0100

    gender graph added

--- Cancer-Dashboard/Server/Overview Graphs.R
+++ Cancer-Dashboard/Server/Overview Graphs.R
@@ -11,22 +11,24 @@ output$scotland_info_graph_server <- renderPlotly({
               type = 'scatter',
               mode = 'lines') %>% 
       layout(xaxis = list(title = "Year"),
-             yaxis = list(title = input$datatype_input))
+             yaxis = list(title = input$graphtype_input))
    
 })
-# 
-# if (input$graphType == "line") {
-#   plot_ly(x = ~ Year,
-#           y = ~ AllAges,
-#           type = 'scatter',
-#           mode = 'lines') %>% 
-#     layout(xaxis = list(title = "Year"),
-#            yaxis = list(title = input$datatype_input))
-# } else if (input$graphType == "bar") {
-#   plot_ly(x = ~ Year,
-#           y = ~ AllAges,
-#           type = 'bar')%>% 
-#     layout(xaxis = list(title = "Year"),
-#            yaxis = list(title = input$datatype_input))
-#   
-# }
+
+output$scotland_gender_graph_server <- renderPlotly({
+  Cancer_Full_Data <- Cancer_Full_Data %>% 
+    select(-CancerSiteICD10Code) %>% 
+    filter(CancerSite == "All cancer types") %>% 
+    filter(Sex != "All") %>% 
+    select(Year, Sex, AllAges, CrudeRate, EASR, WASR, StandardisedRatio, HBName, DataType) %>% 
+    filter(HBName == input$hb_name) %>% select(-HBName) %>% 
+    filter(DataType == input$datatype_input) %>% 
+    plot_ly(x = ~ Year,
+            y = ~ get(input$graphtype_input),
+            color = ~ Sex, colors = gender_palette,
+            type = 'scatter',
+            mode = 'lines') %>% 
+    layout(xaxis = list(title = "Year"),
+           yaxis = list(title = input$graphtype_input))
+  
+})
